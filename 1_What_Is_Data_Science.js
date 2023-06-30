248600f629ba2b12ec6042ad71c325387aaa72ea
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-06-30 18:31:37 +0300

    Create 1_What_Is_Data_Science.js

---
+++ 1_What_Is_Data_Science.js
@@ -0,0 +1,34 @@
+Data science is a multidisciplinary field that uses scientific methods, processes, algorithms, and systems to extract knowledge and insights from structured and unstructured data. It combines mathematics and statistics, specialized programming, advanced analytics, artificial intelligence (AI), and machine learning with specific domain expertise to uncover actionable insights hidden in an organization's data ibm.com.
+
+The goal of data science is to guide decision-making and strategic planning by interpreting data and providing actionable recommendations. This is achieved through a lifecycle that typically involves the following stages:
+
+Data Ingestion: This is the initial stage of data collection from various sources, including structured and unstructured data. The methods used can vary from manual entry to web scraping and real-time streaming data from systems and devices ibm.com.
+Data Storage and Processing: This stage involves considering different storage systems based on the type of data that needs to be captured. Data management teams help set standards around data storage and structure, which facilitate workflows around analytics, machine learning, and deep learning models. This stage includes cleaning data, deduplicating, transforming, and combining the data using ETL (extract, transform, load) jobs or other data integration technologies ibm.com.
+Data Analysis: In this stage, data scientists conduct an exploratory data analysis to examine biases, patterns, ranges, and distributions of values within the data. This exploration drives hypothesis generation for A/B testing and allows analysts to determine the data’s relevance for use within modeling efforts for predictive analytics, machine learning, and/or deep learning ibm.com.
+Data science is one of the fastest-growing fields across every industry due to the accelerating volume of data sources and subsequently, data. As a result, the role of the data scientist has become increasingly important, as organizations heavily rely on them to improve business outcomes ibm.com.
+
+
+
+
+
+
+MORE EXPLANANTION
+*********************************************************************************************************
+
+
+Data science is an interdisciplinary field that combines scientific methods, algorithms, and systems to extract knowledge and insights from structured and unstructured data. It involves using various techniques, tools, and technologies to analyze large volumes of data and uncover meaningful patterns, trends, and relationships.
+
+Data science encompasses a wide range of activities, including data collection, data cleaning and preprocessing, exploratory data analysis, statistical modeling, machine learning, data visualization, and interpretation of results. It involves applying mathematical and statistical concepts, programming skills, and domain knowledge to solve complex problems and make data-driven decisions.
+
+The main goal of data science is to extract actionable insights, predictions, and recommendations from data to drive business value, improve processes, optimize performance, and gain a deeper understanding of various phenomena. It has applications in various domains, including finance, healthcare, marketing, e-commerce, social media, telecommunications, and many others.
+
+Data scientists employ a combination of skills, including statistical analysis, programming (often using languages such as Python or R), machine learning, data visualization, data mining, and domain expertise. They use tools and frameworks like Python libraries (NumPy, pandas, scikit-learn), R programming, SQL databases, Hadoop, Spark, and other technologies to manipulate, analyze, and model data.
+
+In summary, data science involves extracting knowledge and insights from data through a combination of techniques and tools to drive data-informed decision-making and solve complex problems across various industries and domains.
+
+
+
+
+
+
+  
