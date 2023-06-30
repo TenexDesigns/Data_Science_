c9bf0036b7bbb6f70bec7a2af5fbc7bd60db7909
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-06-30 16:26:53 +0300

    Create 1_Data_Science.js

---
+++ 1_Data_Science.js
@@ -0,0 +1,39 @@
+Data science - Is a process of getting insighs from data using technology
+
+
+You dont need machine learning and deep lerning to be a data scientist ---> You can do data science in excel ,by getting insings from bar chats in excel. That is what people used to do in the past
+But in the last few years , the volum of data has increased too much because of evolution of internet, smart devices and social media. So microsoft excel can not work on this large valum of data, because that data is no longer data , it is BIG DATA.
+For big data , you need advanced technology and tools such as
+        Python and R                       - Languages Data scientis use to perform data analisis 
+        Apache haddop and Apache spark     - For data storage and distributed computing
+        matplotlib,powerBI,tableau,jupiter - For data visuslaization
+        TensorFlow,pytorch, sckit          - For deep leraning
+
+These technologies and tools help you analize and get insights from big data in a convenient way.
+
+  DATA SCEIENCE PROCESS
+
+
+1.Understand the bussines problem
+
+2.Data collection
+
+3.Data Cleaning and Exploaration
+
+4.Build a model -machine leraning
+
+5.Deeploy and collect insigts from model- The model could be doing predictive analis like deciding price of inventorut, user recommendations,product and video recomendations
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
